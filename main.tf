9945f7a15471931b97c5c368302818fa401d3e40
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2025-01-25 00:45:15 +0000

    ignore okd ns ann

--- main.tf
+++ main.tf
@@ -13,7 +13,10 @@ resource "kubernetes_namespace_v1" "flux-namespace" {
   lifecycle {
     ignore_changes = [
       metadata[0].annotations["cattle.io/status"],
-      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"]
+      metadata[0].annotations["lifecycle.cattle.io/create.namespace-auth"],
+      metadata[0].annotations["openshift.io/sa.scc.mcs"],
+      metadata[0].annotations["openshift.io/sa.scc.supplemental-groups"],
+      metadata[0].annotations["openshift.io/sa.scc.uid-range"]
     ]
   }
 }
